872d4d4dd61d28ab12cbb828e52af23ab32ce632
Vivek Kejriwal <56736753+VivekKejriwal@users.noreply.github.com>
2021-02-10 12:41:22 +0530

    Update Albumentations.py

--- models/Albumentations.py
+++ models/Albumentations.py
@@ -1,4 +1,4 @@
-from albumentations import Compose, RandomCrop, Normalize, HorizontalFlip, Resize,Rotate, Cutout
+from albumentations import Compose, RandomCrop, Normalize, HorizontalFlip, Resize,Rotate, Cutout, PadIfNeeded
 from albumentations.augmentations.transforms import CoarseDropout
 from albumentations.pytorch import ToTensor
 import numpy as np
@@ -8,15 +8,13 @@ class album_compose():
 
     def __init__(self):
         self.albumentation_transforms = Compose([
-            Rotate((-7.0, 7.0)),
+            PadIfNeeded(min_height=36, min_width=36)
             Cutout(num_holes=4),
             CoarseDropout(),
             # RandomSizedCrop':{'height':32,'width':32,'min_max_height':[28,28]},
-            # RandomCrop(10,10),
+            RandomCrop(32,32),
             HorizontalFlip(),
-            Normalize(
-                mean=[0.5,0.5,0.5], std=[0.5, 0.5, 0.5]
-            ), ToTensor()])
+            Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)), ToTensor()])
 
     def __call__(self, img):
         img = np.array(img)
